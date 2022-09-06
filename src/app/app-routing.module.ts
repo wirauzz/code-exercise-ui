43108049baad8ac7055245e27ed73c464c1b106e
wilson.ramirez <wilson.ramirez@truextend.com>
2022-09-06 14:50:33 -0400

    [EX-3] Added view students detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CoursesPageComponent } from './courses/courses-page/courses-page.component';
+import { StudentsPageComponent } from './students/students-page/students-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'courses', component: CoursesPageComponent },
+  { path: 'students', component: StudentsPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
